524006696bfc9b8d3b06741a2b6b7810695b3e03
Silveron19 <silverterkristian@gmail.com>
2023-05-27 13:34:58 +0800

    postgres database implemented

---
+++ src/server.js
@@ -0,0 +1,33 @@
+const Hapi = require('@hapi/hapi');
+const notes = require('./api/notes');
+const NotesValidator = require('./validator/notes');
+const NotesService = require('./services/postgres/NotesService');
+
+require('dotenv').config();
+
+const init = async () => {
+  const notesService = new NotesService();
+
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+      validator: NotesValidator,
+    },
+  });
+
+  await server.start();
+  console.log(`Server sedang berjalan pada ${server.info.uri}`);
+};
+
+init();
